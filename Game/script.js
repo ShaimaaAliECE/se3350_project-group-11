65ad34061f4fd71bcf0e21c59e78f134d60d4847
dannymccolm19 <dmccolm2@gmail.com>
2022-02-01 08:58:51 -0500

    Improved the code
    Cleaned up and shortened the game code a ton. Whole game works now. Only thing missing is feedback and instructions

--- Game/script.js
+++ Game/script.js
@@ -2,42 +2,25 @@
 
 //Start button/restart button
 const startButton = document.getElementById('start-btn')
-//Button for advancing after getting step complete
+//Button for advancing after getting step complete. Not used currently
 const nextButton = document.getElementById('next-btn')
 
-//Constants for containers for the numbers at each step, probably unecisarry I just built off the existing code
-const questionContainerElement = document.getElementById('question-container')
-const questionContainerElement2 = document.getElementById('question-container2')
-const questionContainerElement3 = document.getElementById('question-container3')
-const questionContainerElement4 = document.getElementById('question-container4')
-const questionContainerElement5 = document.getElementById('question-container5')
-const questionContainerElement6 = document.getElementById('question-container6')
+
+var questionContainerElements = [document.getElementById('question-container'),  document.getElementById('question-container2'), document.getElementById('question-container3'),  document.getElementById('question-container4'),document.getElementById('question-container5'), document.getElementById('question-container6') ]
 //const questionElement = document.getElementById('question')
 
-//Constant for the set of numbers at each step
-const answerButtonsElement = document.getElementById('answer-buttons')
-const answerButtonsElement2 = document.getElementById('answer-buttons2')
-const answerButtonsElement3 = document.getElementById('answer-buttons3')
-const answerButtonsElement4 = document.getElementById('answer-buttons4')
-const answerButtonsElement5 = document.getElementById('answer-buttons5')
-const answerButtonsElement6 = document.getElementById('answer-buttons6')
+var answerButtonsElements = [document.getElementById('answer-buttons'), document.getElementById('answer-buttons2'), document.getElementById('answer-buttons3'), document.getElementById('answer-buttons4'), document.getElementById('answer-buttons5'), document.getElementById('answer-buttons6')]
 
 //Keeps track of what step the user is on
 var step
 
-//Arrays for each set of numbers. nums1 is the randomly generated one and all others are populated with some subset of nums1
-var nums1 = []
-var nums2 = []
-var nums3 = []
-var nums4 = []
-var nums5 = []
-var nums6 = []
-var nums7 = []
+//Arrays for each array of numbers at each step.
+var numbers = []
 
 //An array to store sorted arrays. To check if the user ordered correctly
 var sorted = []
 
-//For step 3. Is either 3 or 2 for when the spplit is uneven
+//For step 3. Is either 3 or 4 for when the split is uneven. 
 var split
 
 //Just a variable to be incremented for the sorting step
@@ -45,19 +28,19 @@ var correct = 0
 
 
 startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  setNextQuestion()
-})
+/*nextButton.addEventListener('click', () => {
+  setNextLevel()
+})*/
 
 function startGame() {
   step = 1
   resetState()
   startButton.classList.add('hide')
-  questionContainerElement.classList.remove('hide')
+  questionContainerElements[0].classList.remove('hide')
 
   //Clears the array if it is a restart
-  for(x=0; x<10;x++){
-    nums1.pop()
+  for(x=0; x<7;x++){
+    numbers.pop()
   }
   
   fillArray()
@@ -69,156 +52,151 @@ function getRndInteger(min, max) {
 }
 
 function fillArray() {
+  var nums = []
   //Keeps adding to array until it is 10 long
-  while (nums1.length < 11){
+  while (nums.length < 10){
     var x = getRndInteger(1,21)
     var e = false
 
     //Checks if new integer is already in array. Does not add if so
-    for (y in nums1){
-      if (x == nums1[y]){
+    for (y in nums){
+      if (x == nums[y]){
        var e = true
       }
     }
     if (e == false){
-      nums1.push(x)
+      nums.push(x)
     }
   }
-  showQuestion(nums1)
+  numbers.push(nums)
+  showQuestion(0)
 }
 
 
 
 
-function setNextQuestion() {
+function setNextLevel(x) {
+  var nums = []
   step++
   resetState()
 
-  //Splits the initial array in half
-  for(i=0; i<5;i++){
-    nums2.push(nums1[i])
-    nums5.push(nums1[i+5])
+  if (x == 1){
+    //Takes the top half of the ten numbers
+    for(i=0; i<5;i++){
+      nums.push(numbers[0][i])
+    }
+    questionContainerElements[1].classList.remove('hide')
+    questionContainerElements[4].classList.remove('hide')
+    numbers.push(nums)
   }
 
-  if (step ==2){
-    questionContainerElement2.classList.remove('hide')
-    questionContainerElement5.classList.remove('hide')
-    showQuestion(nums2)
-  }
-  if (step ==3){
-    //Stores top 2 of the second array
+  else if (x == 2){
+    var nums2 = []
+    //takes the top two and bottom three of the top half
     for(i=0; i<2;i++){
-      nums3.push(nums2[i])
+      nums.push(numbers[1][i])
+      nums2.push(numbers[1][i+2])
     }
-    questionContainerElement3.classList.remove('hide')
+    nums2.push(numbers[1][4])
+    questionContainerElements[2].classList.remove('hide')
+    questionContainerElements[3].classList.remove('hide')
+    numbers.push(nums)
     split = 3
-    showQuestion2(nums3)
+    showQuestion(2)
+    numbers.push(nums2)
+    //Sorts the top half
+    sorted = numbers[1].sort(function(a, b){return a - b})
+    split = 4
     step++
+    x++
   }
 
-  if (step ==4){
-    //Stores bottom 3 of the second array
-    for(i=2; i<5;i++){
-      nums4.push(nums2[i])
+  else if (x == 4){
+    //Takes the bottom half of the ten numbers
+    for(i=0; i<5;i++){
+      nums.push(numbers[0][i+5])
     }
-    questionContainerElement4.classList.remove('hide')
-    split = 4
-    showQuestion2(nums4)
-    //Sorts the second array
-    sorted = nums2.sort(function(a, b){return a - b})
+    numbers.push(nums)
   }
-  if (step ==5){
-    showQuestion(nums5)
-  }
-  if (step ==6){
+
+  else if (x == 5){
+    var nums2 = []
+    //takes the top two and bottom three of the bottom half
     for(i=0; i<2;i++){
-      nums6.push(nums5[i])
+      nums.push(numbers[4][i])
+      nums2.push(numbers[4][i+2])
     }
-    questionContainerElement3.classList.remove('hide')
+    nums2.push(numbers[4][4])
+    questionContainerElements[2].classList.remove('hide')
+    questionContainerElements[3].classList.remove('hide')
+    numbers.push(nums)
     split = 3
-    showQuestion2(nums6)
-    step++
-  }
-  if (step ==7){
-    for(i=2; i<5;i++){
-      nums7.push(nums5[i])
-    }
-    questionContainerElement4.classList.remove('hide')
+    showQuestion(5)
+    numbers.push(nums2)
+    //Sorts the bottom half
+    sorted = numbers[4].sort(function(a, b){return a - b})
     split = 4
-    showQuestion2(nums7)
-    sorted = nums5.sort(function(a, b){return a - b})
+    step++
+    correct = 0
+    x++
+  }
+
+  else if (x == 7){
+    //Sorts all ten numbers
+    sorted = numbers[0].sort(function(a, b){return a - b})
     correct = 0
   }
-  if (step ==8){
-    Array.from(answerButtonsElement2.children).forEach(button => {
-      button.addEventListener('click', selectAnswer2)
-    })
 
-    Array.from(answerButtonsElement5.children).forEach(button => {
-      button.addEventListener('click', selectAnswer2)
-    })
-
-    sorted = nums1.sort(function(a, b){return a - b})
-    correct = 0
-  
-  }
+  showQuestion(x)
 
 }
 
 //Shows the numbers on the screen. Works for multiuple steps because of the naming scheme of the buttons
-function showQuestion(numbers) {
-  var x = 0
-  for ( y in numbers){
-    const button = document.getElementById(`btn${step}${x}`)
-    button.innerText = numbers[y]
-    x++
-    button.addEventListener('click', selectAnswer)
+function showQuestion(x) {
+
+  if (x<7){
+    const nums = numbers[x]
+    var z = 0
+    for ( y in nums){
+      var button
+      if (x==2 || x==3 || x==5 || x==6){ 
+            button = document.getElementById(`btn${split}${z}`)
+            button.addEventListener('click', selectAnswer)
+          }
+      else { 
+            button = document.getElementById(`btn${step}${z}`)
+            button.addEventListener('click', selectAnswer)
+          }
+      button.innerText = nums[y]
+      z++
+      button.addEventListener('click', selectAnswer)
+    }
+  }
+  else {
+    Array.from(answerButtonsElements[1].children).forEach(button => {
+      button.addEventListener('click', selectAnswer)
+    })
+    Array.from(answerButtonsElements[4].children).forEach(button => {
+      button.addEventListener('click', selectAnswer)
+    })
   }
 
 }
 
-//Same as  the above but for uneven array
-function showQuestion2(numbers) {
-  var x = 0
-  for ( y in numbers){
-    const button = document.getElementById(`btn${split}${x}`)
-    button.innerText = numbers[y]
-    x++
-    button.addEventListener('click', selectAnswer2)
-  }
-
-}
 
 //Removes click listeners from previous steps
 function resetState() {
   clearStatusClass(document.body)
-  if (step ==2||step ==1){
-    Array.from(answerButtonsElement.children).forEach(button => {
-      clearStatusClass(button)
-      button.removeEventListener('click', selectAnswer)
-    })
-  }
-  if (step ==3){
-    Array.from(answerButtonsElement2.children).forEach(button => {
-      clearStatusClass(button)
-      button.removeEventListener('click', selectAnswer)
-      button.innerText = ""
-    })
-  }
+  var y
+  if (step ==2||step ==1||step == 8){y = 0}
+  if (step ==3){y=1}
+  if (step ==6){y=4}
 
-  if (step ==6){
-    Array.from(answerButtonsElement5.children).forEach(button => {
+  if (step <4||step==6||step==8){
+    Array.from(answerButtonsElements[y].children).forEach(button => {
       clearStatusClass(button)
       button.removeEventListener('click', selectAnswer)
-      button.innerText = ""
-    })
-  }
-
-  if (step ==8){
-    Array.from(answerButtonsElement.children).forEach(button => {
-      clearStatusClass(button)
-      button.removeEventListener('click', selectAnswer)
-      button.innerText = ""
+      if (step >2){ button.innerText = ""}
     })
   }
  
@@ -226,109 +204,81 @@ function resetState() {
 
 }
 
-//Checks if the selected answer is correct. For splitting
+//Checks if the selected answer is correct.
 function selectAnswer(e) {
   const selectedButton = e.target
   const num = selectedButton.innerText
-  if (step ==1){
-    if (num == nums1[5]){
-      setStatusClass(document.body, num)
-      setStatusClass(selectedButton, num)
-      nextButton.classList.remove('hide')
-    }
-    else{Array.from(answerButtonsElement.children).forEach(button => {
-      setStatusClass(button, button.dataset.num)
-      startButton.innerText = 'Restart'
-      startButton.classList.remove('hide')
-    })}
+  
+  if (num == numbers[0][5]&&step==1){
+    //setStatusClass(document.body, num)
+    //setStatusClass(selectedButton, num)
+    setNextLevel(1)
   }
-  if (step ==2){
-    if (num == nums2[2]){
-      setStatusClass(document.body, num)
-      setStatusClass(selectedButton, num)
-      nextButton.classList.remove('hide')
-    }
-    else if (num == nums2[3]){
-      setStatusClass(document.body, num)
-      setStatusClass(selectedButton, num)
-      nextButton.classList.remove('hide')
-    }
-    else{Array.from(answerButtonsElement2.children).forEach(button => {
-      setStatusClass(button, button.dataset.num)
-      startButton.innerText = 'Restart'
-      startButton.classList.remove('hide')
-    })}
+  if ((num == numbers[1][2]||num == numbers[1][3])&&step==2){
+    setNextLevel(2)
   }
 
-  if (step ==5){
-    if (num == nums5[2]){
-      setStatusClass(document.body, num) 
-      setStatusClass(selectedButton, num)
-      nextButton.classList.remove('hide')
-    }
-    else if (num == nums5[3]){
-      setStatusClass(document.body, num)
-      setStatusClass(selectedButton, num)
-      nextButton.classList.remove('hide')
-    }
-    else{Array.from(answerButtonsElement2.children).forEach(button => {
-      setStatusClass(button, button.dataset.num)
-      startButton.innerText = 'Restart'
-      startButton.classList.remove('hide')
-    })}
-  }
-}
-
-//Same as above but for ordering not splitting
-function selectAnswer2(e) {
-  const selectedButton = e.target
-  const num = selectedButton.innerText
   if (step ==4){
-    const num2 = Array.from(answerButtonsElement2.children)[correct]
+    const num2 = Array.from(answerButtonsElements[1].children)[correct]
     if (num == sorted[0]){
       num2.innerText = num
       correct++
       sorted.shift()
-      sorted.shift()
       selectedButton.innerText = ""
     }
     if (sorted.length == 0){
-      questionContainerElement3.classList.add('hide')
-      questionContainerElement4.classList.add('hide')
-      nextButton.classList.remove('hide')
-    }
-  }
-  if (step ==7){
-    const num2 = Array.from(answerButtonsElement5.children)[correct]
-    if (num == sorted[0]){
-      num2.innerText = num
-      correct++
-      sorted.shift()
-      sorted.shift()
-      sorted.shift()
-      sorted.shift()
-      selectedButton.innerText = ""
-    }
-    if (sorted.length == 0){
-      questionContainerElement3.classList.add('hide')
-      questionContainerElement4.classList.add('hide')
-      nextButton.classList.remove('hide')
+      questionContainerElements[2].classList.add('hide')
+      questionContainerElements[3].classList.add('hide')
+      setNextLevel(4)
     }
   }
 
-  if (step ==8){
-    const num2 = Array.from(answerButtonsElement.children)[correct]
+  if ((num == numbers[4][2]||num == numbers[4][3])&&step==5){
+    setNextLevel(5)
+  }
+
+  if (step ==7){
+    const num2 = Array.from(answerButtonsElements[4].children)[correct]
     if (num == sorted[0]){
       num2.innerText = num
       correct++
       sorted.shift()
+      selectedButton.innerText = ""
     }
     if (sorted.length == 0){
-      questionContainerElement2.classList.add('hide')
-      questionContainerElement5.classList.add('hide')
-      nextButton.classList.remove('hide')
+      questionContainerElements[2].classList.add('hide')
+      questionContainerElements[3].classList.add('hide')
+      setNextLevel(7)
     }
   }
+
+  if (step > 7){
+ 
+    const num2 = Array.from(answerButtonsElements[0].children)[correct]
+    if (num == sorted[0]){
+      num2.innerText = num
+      correct++
+      sorted.shift()
+      selectedButton.innerText = ""
+    }
+    if (sorted.length == 0){
+      questionContainerElements[1].classList.add('hide')
+      questionContainerElements[4].classList.add('hide')
+    }
+  }
+    /*else{Array.from(answerButtonsElements[0].children).forEach(button => {
+      //setStatusClass(button, button.dataset.num)
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+    })}
+    else{
+      Array.from(answerButtonsElements[1].children).forEach(button => {
+      //setStatusClass(button, button.dataset.num)
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+    })}*/
+  
+  
 }
 
 function setStatusClass(element, correct) {
